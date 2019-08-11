87ce956ca0b13238f0cb1812d4a581dd5c580ab0
gangking <gangking@foxmail.com>
2019-08-11 18:32:14 +0800

    优化

--- src/routers/index.js
+++ src/routers/index.js
@@ -7,15 +7,16 @@ import mineRouter from './mine'
 Vue.use(Router)
 
 export default new Router({
-  mode: 'history',
-  base: process.env.BASE_URL,
-  routes: [
-    movieRouter,
-    cinameRouter,
-    mineRouter,
-    {
-      path: '/*',
-      redirect: '/movie'
-    }
-  ]
+    mode: 'history',
+    //   base: process.env.BASE_URL,
+    base: 'miaomiao',
+    routes: [
+        movieRouter,
+        cinameRouter,
+        mineRouter,
+        {
+            path: '/*',
+            redirect: '/movie'
+        }
+    ]
 })
