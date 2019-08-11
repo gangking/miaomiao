7db230ee19b7f014ec183b41499bb4ca8e660e98
gangking <gangking@foxmail.com>
2019-08-11 11:27:54 +0800

    add setData

--- src/components/Search/index.vue
+++ src/components/Search/index.vue
@@ -3,28 +3,21 @@
     <div class="search_input">
       <div class="search_input_wrapper">
         <i class="iconfont icon-sousuo"></i>
-        <input type="text">
+        <input type="text"
+               v-model="message">
       </div>
     </div>
     <div class="search_result">
       <h3>电影/电视剧/综艺</h3>
       <ul>
-        <li>
-          <div class="img"><img src="/images/movie_1.jpg"></div>
+        <li v-for="(item) in moviesList"
+            :key="item.id">
+          <div class="img"><img :src="item.img | setWH('128.180')"></div>
           <div class="info">
-            <p><span>无名之辈</span><span>8.5</span></p>
-            <p>A Cool Fish</p>
-            <p>剧情,喜剧,犯罪</p>
-            <p>2018-11-16</p>
-          </div>
-        </li>
-        <li>
-          <div class="img"><img src="/images/movie_1.jpg"></div>
-          <div class="info">
-            <p><span>无名之辈</span><span>8.5</span></p>
-            <p>A Cool Fish</p>
-            <p>剧情,喜剧,犯罪</p>
-            <p>2018-11-16</p>
+            <p><span>{{item.nm}}</span><span>{{item.sc}}</span></p>
+            <p>{{item.enm}}</p>
+            <p>{{item.cat}}</p>
+            <p>{{item.pubDesc}}</p>
           </div>
         </li>
       </ul>
@@ -33,8 +26,49 @@
 </template>
 
 <script>
+import { clearTimeout, setTimeout } from 'timers';
 export default {
-  name: 'Search'
+  name: 'Search',
+  data () {
+    return {
+      message: '',
+      moviesList: []
+    }
+  },
+  watch: {
+    message (val) {
+      var that = this;
+      // 取消上一次请求
+      this.cancelRequest();
+
+      this.axios.get('/api/searchList?cityId=10&kw=' + val, {
+        cancelToken: new this.axios.CancelToken(function (c) {
+          that.source = c;
+        })
+      }).then((res) => {
+        console.log(res.data.data)
+        var msg = res.data.msg;
+        var data = res.data.data;
+        if (msg === 'ok') {
+          this.moviesList = data.movies.list;
+        }
+      }).catch((err) => {
+        if (this.axios.isCancel(err)) {
+          console.log('Rquest canceled', err.message); //请求如果被取消，这里是返回取消的message
+        } else {
+          //handle error
+          console.log(err);
+        }
+      })
+    }
+  },
+  methods: {
+    cancelRequest () {
+      if (typeof this.source === 'function') {
+        this.source('终止请求')
+      }
+    },
+  }
 }
 </script>
 
